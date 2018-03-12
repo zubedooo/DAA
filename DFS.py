349f677baf9b51577fb5eef98c485cf505bed7c4
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-12 22:43:12 +0530

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,20 @@
+graph={
+        1:[2,3],
+        2:[1,3,4,5],
+        3:[1,2,6,7],
+        4:[2,5,8,9],
+        5:[2,4,9],
+        6:[3,7],
+        7:[3,6],
+        8:[4,9],
+        9:[4,5,8,10],
+        10:[9]
+}
+visited=[0]*10
+def dfs(node):
+        visited[node-1]=1
+        print node
+        for k in graph[node]:
+                if visited[k-1]==0:
+                        dfs(k)
+dfs(1)
