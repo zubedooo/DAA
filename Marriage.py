75c0497ac67908263a1b54bb40ef82b13d49d13a
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-13 19:34:54 +0530

    Update Marriage.py

--- Marriage.py
+++ Marriage.py
@@ -6,24 +6,17 @@ takenwomen=[]
 final={}
 while freemen!=[]:
     for i in freemen:
-        #print("MAN=",i)
         listofw=menpref.get(i)
-        #print("List of women for ",i,listofw)
         for j in listofw:
             if j not in takenwomen:
                 final[j]=i
                 freemen.remove(i)
-                #print("list of freemen",freemen)
                 takenwomen.append(j)
-                #print("final list= ",final)
                 break
             else:
                 listofm=womenpref.get(j)
-                #print("list of men for ",j,listofm)
                 r=final.get(j)
-                #print("Previously matched man",r)
                 p=listofm.index(i)
-                #print(p)
                 q=listofm.index(r)
                 if p<q:
                     final[j]=i
@@ -32,7 +25,6 @@ while freemen!=[]:
                     takenwomen.append(j)
                     break
 print(final)
-#print("End")
 print("The program ran for:")
 print(time.clock())
 print("seconds")
