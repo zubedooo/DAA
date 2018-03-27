791403eeb829f10a14c3f8293ced470d5c35a221
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-03-27 15:28:19 +0530

    Create Prims.py

---
+++ Prims.py
@@ -0,0 +1,40 @@
+def popmin(pqueue):
+    lowest = 1000
+    keylowest = None
+    for key in pqueue:
+        if pqueue[key] < lowest:
+            lowest = pqueue[key]
+            keylowest = key
+    del pqueue[keylowest]
+    return keylowest
+ 
+def prim(graph, root):
+    pred = {}
+    key = {}
+    pqueue = {}
+ 
+    for v in graph:
+        pred[v] = -1
+        key[v] = 1000
+    key[root] = 0
+    for v in graph:
+        pqueue[v] = key[v]
+     
+    while pqueue:
+        u = popmin(pqueue)
+        for v in graph[u]:
+            if v in pqueue and graph[u][v] < key[v]:
+                pred[v] = u
+                key[v] = graph[u][v]
+                pqueue[v] = graph[u][v]
+    return pred
+ 
+graph = {0 : {1:6, 2:8},
+1 : {4:11},
+2 : {3: 9},
+3 : {},
+4 : {5:3},
+5 : {2: 7, 3:4}}
+ 
+pred = prim(graph, 0)
+for v in pred: print "%s: %s" % (v, pred[v])
