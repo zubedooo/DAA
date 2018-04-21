7504bb9df68b738775bf664e62a8ba232e32f081
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-22 00:11:18 +0530

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,13 +1,21 @@
 import time
-nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
-distances = {
-    'B': {'A': 5, 'D': 1, 'G': 2},
-    'A': {'B': 5, 'D': 3, 'E': 12, 'F' :5},
-    'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
-    'G': {'B': 2, 'D': 1, 'C': 2},
-    'C': {'G': 2, 'E': 1, 'F': 16},
-    'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
-    'F': {'A': 5, 'E': 2, 'C': 16}}
+
+str_nodes=input("Enter the nodes\n").upper() 
+nodes=str_nodes.split(" ")
+print (nodes)
+
+distances=dict()
+dummy_dict=dict()
+
+for i in nodes:
+    dummy_dict={}
+    noOfAttachments=int(input("How many nodes are attached to %c "%(i)))
+    for j in range(noOfAttachments):
+        a=input("Enter one node %c is attached to "%(i)).upper()
+        b=int(input("Enter the weight"))
+        dummy_dict[a]=b
+    distances[i]=dummy_dict
+print (distances)
 
 unvisited = {node: None for node in nodes} #using None as +inf
 visited = {}
@@ -30,4 +38,4 @@ while True:
 
 end=time.clock()
 print(visited)
-print "The program ran for: ",end-start,"seconds"
+print ("The program ran for: ",end-start,"seconds")
