e66642cfa0e3d5b57475e801b59fd736c1ce8977
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-23 00:15:41 +0530

    Create IntervalScheduling.py

---
+++ IntervalScheduling.py
@@ -0,0 +1,19 @@
+def printMaxActivities(s,f):
+    n = len(f)
+    print ("The following activities are selected")
+    i = 0
+    print (i)
+    for j in range(n):
+        if s[j] >= f[i]:
+            print (j)
+            i = j
+s = []
+no=int(input("Enter size: "))
+for u in range(no):
+        s.insert(u,int(input("Enter Values:")))
+print (s)
+f = []
+no1=int(input("Enter size: "))
+for v in range(no1):
+        f.insert(v,int(input("Enter Values: ")))
+printMaxActivities(s,f)
