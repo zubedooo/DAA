b631d327365aa39e74c636e5e0ac181d25cda8f1
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-23 00:23:37 +0530

    Update IntervalScheduling.py

--- IntervalScheduling.py
+++ IntervalScheduling.py
@@ -1,3 +1,4 @@
+import time
 def printMaxActivities(s,f):
     n = len(f)
     print ("The following activities are selected")
@@ -11,9 +12,13 @@ s = []
 no=int(input("Enter size: "))
 for u in range(no):
         s.insert(u,int(input("Enter Values:")))
-print (s)
+print(s)
 f = []
 no1=int(input("Enter size: "))
 for v in range(no1):
         f.insert(v,int(input("Enter Values: ")))
+print(f)
+start=time.clock()
 printMaxActivities(s,f)
+end=time.clock()
+print("The program ran for: ",end-start,"seconds")
