b1e15992b84f49f3284be763d4a576f575a81f38
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-24 17:04:51 +0530

    Update Kruskal.c

--- Kruskal.c
+++ Kruskal.c
@@ -69,28 +69,18 @@ void KruskalMST(struct Graph* graph)
         struct Edge next_edge = graph->edge[i++];
         int x = find(subsets, next_edge.src);
         int y = find(subsets, next_edge.dest);
- 
-        // If including this edge does't cause cycle,
-        // include it in result and increment the index 
-        // of result for next edge
         if (x != y)
         {
             result[e++] = next_edge;
             Union(subsets, x, y);
         }
-        // Else discard the next_edge
     }
- 
-    // print the contents of result[] to display the
-    // built MST
     printf("Following are the edges in the constructed MST\n");
     for (i = 0; i < e; ++i)
         printf("%d -- %d == %d\n", result[i].src, result[i].dest,
                                                  result[i].weight);
     return;
 }
- 
-// Driver program to test above functions
 int main()
 {
     /* Let us create following weighted graph
@@ -101,37 +91,24 @@ int main()
         |      \ |
         2--------3
             4       */
-    int V = 4;  // Number of vertices in graph
-    int E = 5;  // Number of edges in graph
+    int V = 4;
+    int E = 5;
     struct Graph* graph = createGraph(V, E);
- 
- 
-    // add edge 0-1
     graph->edge[0].src = 0;
     graph->edge[0].dest = 1;
     graph->edge[0].weight = 10;
- 
-    // add edge 0-2
     graph->edge[1].src = 0;
     graph->edge[1].dest = 2;
     graph->edge[1].weight = 6;
- 
-    // add edge 0-3
     graph->edge[2].src = 0;
     graph->edge[2].dest = 3;
     graph->edge[2].weight = 5;
- 
-    // add edge 1-3
     graph->edge[3].src = 1;
     graph->edge[3].dest = 3;
     graph->edge[3].weight = 15;
- 
-    // add edge 2-3
     graph->edge[4].src = 2;
     graph->edge[4].dest = 3;
     graph->edge[4].weight = 4;
- 
     KruskalMST(graph);
- 
     return 0;
 }
