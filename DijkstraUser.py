4f124a947f6d61a568d2d9950fccbbcb13b601f3
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-26 00:45:54 +0530

    Update DijkstraUser.py

--- DijkstraUser.py
+++ DijkstraUser.py
@@ -1,33 +1,55 @@
 import time
-graph={}
-no=int(input("Enter Number Of Nodes: "))
-for i in range(no):
-  graph[i]={}
-  print ("Enter Number Of Nodes Connected to",i)
-  n=int(input())
-  for j in range(n):
-    x,w=map(int,input("Enter Node and Weight\n").split())
-    graph[i][x]=w
-print (graph)    
-def Dijkstra(s):
-  d,visited=[999]*len(graph),[0]*len(graph)
-  d[s-1]=0
-  visited[s-1]=s-1
-  vertices=[s]
-  while len(vertices)!=len(graph.keys()):
-    edges={}
-    for u in vertices:
-      for v in graph[u].keys():
-        if visited[v-1]==0:
-          t=d[u-1]+graph[u][v]
-          edges.update({t:[u,v]})
-    min_d=min(edges.keys())
-    node=edges[min_d][1]
-    d[node-1]=min_d
-    vertices.append(node)
-    visited[node-1]=node-1
-  return d
+def addEdge(graph, v1, v2, w):
+	graph.setdefault(v1, {})
+	graph[v1].update({v2:w})
+
+def dijkstra(G, nodes, s):
+	dist, prev = {}, {}
+
+	for v in nodes:
+		dist[v] = float('inf')
+		prev[v] = None
+
+	dist[s] = 0
+
+	while nodes:
+		u = min(nodes, key=dist.get)
+		nodes.remove(u)
+
+		for v in G[u]:
+			alt = dist[u] + G[u][v]
+			if alt < dist[v]:
+				dist[v] = alt
+				prev[v] = u
+
+	return dist,prev
+
+n = int(input('\nEnter number of edges of undirected graph: '))
+g = {}
+nodes = set()
+
+print('u v cost(u,v)')
+for i in range(n):
+	a,b,w = input().split()
+	w = int(w)
+	addEdge(g,a,b,w)
+	addEdge(g,b,a,w)
+	nodes.update([a,b])
+
+src = input('\nEnter source node: ')
+
 start=time.clock()
-print (Dijkstra(1))
+d,p = dijkstra(g, nodes, src)
 end=time.clock()
-print ("The Program Ran for: ",end-start,"seconds")
+path = {}
+
+print('Node\tLowest cost\tPath')
+for i in sorted(d):
+
+	path[i],it = [i],p[i]
+	while it!=None:
+		path[i] = [it] + path[i]
+		it = p[it]
+
+	print(i, '\t', d[i], '\t\t', path[i])
+  print("The program ran for: ",end-start,"seconds")
