cb256df9856589aa8d39fbf3692650007ce70db7
zubedooo <34791155+zubedooo@users.noreply.github.com>
2018-04-26 14:08:44 +0530

    Update Quick.py

--- Quick.py
+++ Quick.py
@@ -1,36 +1,26 @@
 import random
 import time
-def merge(a,b):
-		m=[]
-		c=0
-		while len(a) and len(b):
-			if a[0]<b[0]:
-			    m.append(a.pop(0))
-			else:
-			    c+=len(a)
-			    m.append(b.pop(0))
-		if len(a) == 0:
-			m+=b
-		if len(b) == 0:
-			m+=a
-		return m,c
+def partition(arr,low,high):
+    i = ( low-1 )
+    pivot = arr[high]
+    for j in range(low , high):
+        if   arr[j] <= pivot:
+                     i = i+1;arr[i],arr[j] = arr[j],arr[i]
 
-def sort(arr):
-        if len(arr) <= 1:
-                return arr,0
-        mid = len(arr)//2
-        b,r1 = sort(arr[:mid])
-        c,r2 = sort(arr[mid:])
-        d,c = merge(b,c)
-        return d,(r1+r2+c)
-n=6
-x=[]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return ( i+1 )
+def quickSort(arr,low,high):
+    if low < high:
+        pi = partition(arr,low,high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+arr =[]
+n=10
 for i in range(n):
-     x.append(random.randint(0,n+1))
-print "Initial list: ",x
-start= time.clock()
-s,c= sort(x)
-end= time.clock()
-print "Sorted list: ",s
-print "Number of Invertions: ",c
-print "Time :",end-start
+    arr.append(random.randint(0,100))
+print arr
+start=time.clock()
+quickSort(arr,0,n-1)
+end=time.clock()
+print arr
+print "Time taken to sort array: ", end-start,"seconds"
